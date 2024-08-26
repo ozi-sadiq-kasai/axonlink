717a350f2ebd023948d7f4383027d44a5add6cb5
ozi <ozisadiqkasai@gmail.com>
2024-08-26 23:35:50 +0100

    building

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.use(passport.session())
 // Import routes
 app.use(authRoutes);
 
+if(process.env.NODE_ENV ==='production'){
+ app.use(express.static('client/build'))
+
+ const path = require('path')
+ app.getMaxListeners('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+ })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
